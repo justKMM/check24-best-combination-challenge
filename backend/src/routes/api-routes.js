e48e376aedcc3f14bc89c44aa8cc3832fb77e5a2
kmai <khaiminhmai2@gmail.com>
2024-11-13 23:36:45 +0100

    init front and backend - basic auth service

---
+++ backend/src/routes/api-routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {checkAuthorization} = require('../middlewares/auth-middleware');
+
+/*
+    In this file is the routing for the REST-endpoints under /api managed
+ */
+
+const authApi = require('../apis/auth-api'); //api-endpoints are loaded from separate files
+router.post('/login', authApi.login); //the function decides which request type should be accepted
+router.delete('/login', checkAuthorization(),authApi.logout); //middlewares can be defined in parameters
+router.get('/login', authApi.isLoggedIn); //the function, which handles requests is specified as the last parameter
